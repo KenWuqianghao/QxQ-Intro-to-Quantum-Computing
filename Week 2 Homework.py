1d69d1d8fe29c2de1599bbd35a2a81869d39c1a6
Ken Wu <wuq@asmilan.org>
2021-10-23 18:20:52 +0200

    Week 4

---
+++ Week 2 Homework.py
@@ -0,0 +1,30 @@
+goal_time = 20 
+
+time_1 = 19.4
+
+time_2 = 21.3
+
+time_3 = 20.1
+
+time_4 = 19.7
+
+average_time = (time_1 + time_2 + time_3 + time_4)/4
+
+if average_time <= goal_time:
+
+	print ("Your average time is better than your goal time of 20 minutes.")
+
+else:
+	print("Your average time is worse than your goal time of 20 minutes.") 
+
+x = 2
+
+if x <= 5:
+
+    a = x+ 5
+
+    print(a**2)
+
+elif x**2 > 50:
+
+    print((x-2)**2)
